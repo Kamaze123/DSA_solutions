8c2bf779c4013da0286a649000218adaa4c43521
Kamalesh <kamaleshbabu622@gmail.com>
2026-05-15 14:55:47 +0530

    Add: n-queens - submission-3

---
+++ Data Structures & Algorithms/n-queens/submission-3.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    unordered_set<int> cols;
+    unordered_set<int> posDiag;
+    unordered_set<int> negDiag;
+    vector<vector<string>> ans;
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        backtrack(board, 0, n);
+        return ans;
+    }
+
+    void backtrack(vector<string>& board, int row, int n){
+        if(row == n){
+            ans.push_back(board);
+            return;
+        }
+
+        for(int col = 0; col < n; col++){
+            if(cols.count(col) || posDiag.count(row + col) || negDiag.count(row - col)){
+                continue;
+            }
+
+            board[row][col] = 'Q';
+            cols.insert(col);
+            posDiag.insert(row + col);
+            negDiag.insert(row - col);
+
+            backtrack(board, row + 1, n);
+
+            negDiag.erase(row - col);
+            posDiag.erase(row + col);
+            cols.erase(col);
+            board[row][col] = '.';
+        }
+    }
+};
