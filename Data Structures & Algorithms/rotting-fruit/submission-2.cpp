52866a8647bbbc8191ce2c5b32880a5e93458895
Kamalesh <kamaleshbabu622@gmail.com>
2026-05-18 13:37:21 +0530

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<vector<int>> q;    //{row, col, time}
+        int row = grid.size();
+        int col = grid[0].size();
+        int maxtime = 0;
+
+        vector<vector<int>> dir = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+
+        //vector<vector<bool>> seen(row, vector<bool>(col, false));
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == 2){
+                    q.push({i,j, 0});
+                }
+            }
+        }
+
+        while(!q.empty()){
+            vector<int> vec = q.front();
+            q.pop();
+            int x = vec[0];
+            int y = vec[1];
+            int t = vec[2];
+            t++;
+
+            for(vector<int> d : dir){
+                int nx = x + d[0];
+                int ny = y + d[1];
+
+                if(nx < 0 || ny < 0 || nx >= row || ny >= col){continue;}
+
+                if(grid[nx][ny] == 0 || grid[nx][ny] == 2){continue;}
+
+                maxtime = max(maxtime, t);
+
+                grid[nx][ny] = 2;
+                q.push({nx, ny, t});
+            }
+        }
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == 1){
+                    return -1;
+                }
+            }
+        }
+
+        return maxtime;
+    }
+};
