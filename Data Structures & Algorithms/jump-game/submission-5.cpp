b556440827055a2be2f5c82e7176e217b71905e4
Kamalesh <kamaleshbabu622@gmail.com>
2026-06-06 11:27:34 +0400

    Add: jump-game - submission-5

---
+++ Data Structures & Algorithms/jump-game/submission-5.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int len = nums.size();
+        vector<int> memo(len+1, -1);
+        return rec(nums, memo, 0, len-1);
+    }
+
+    bool rec(vector<int>& nums, vector<int>& memo, int index, int len){
+        if(index == len){
+            return true;
+        }
+
+        if(nums[index] == 0){
+            return false;
+        }
+
+        if(memo[index] != -1){
+            return memo[index];
+        }
+
+        int jump =  nums[index];
+        for(int i = 1; i <= jump; i++){
+            if(rec(nums, memo, index+i, len)){
+                memo[index] = true;
+                return true;
+            }
+        }
+
+        memo[index] = false;
+        return false;
+    }
+};
